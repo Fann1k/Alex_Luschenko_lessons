e085860e84d59e03603d9000e3e319bb10864654
Хантимиров Фанис Минегалиевич <Khantimirov_FM@kvz.ru>
2019-04-04 17:14:08 +0300

    04/04/19

---
+++ 07_циклы_массивы/3/app/script.js
@@ -0,0 +1,3 @@
+for (let i = 200; i > -1; i--) {
+    document.querySelector('#output').innerHTML += ' ' + i;
+}
---
+++ 07_циклы_массивы/3/app/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+for (let i = 200; i < 1; i--) {
+    document.querySelector('#output').innerHTML += i;
+    console.log(11 + i);
+}
